1a4767649ff66d38f025980bfc1a15a811127f60
kiyopikko <piyo326@gmail.com>
2025-01-02 21:00:46 +0900

    feat: /with-search も同様のUIに

--- src/app/with-search/page.tsx
+++ src/app/with-search/page.tsx
@@ -1,9 +1,11 @@
+import { Projects } from "../projects/Projects";
+
 type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 
 const Page = async (props: { searchParams: SearchParams }) => {
   const searchParams = await props.searchParams;
   console.log(searchParams);
-  return <div>サーチページ</div>;
+  return <Projects />;
 };
 
 export default Page;
